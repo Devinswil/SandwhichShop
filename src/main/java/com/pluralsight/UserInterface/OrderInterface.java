e2f2e7277883db8b28325e12ea51386ab0d6fb63
DevinW <devinswilson03@gmail.com>
2024-11-13 15:52:49 -0500

    added error handling in drink and chip methods

--- src/main/java/com/pluralsight/UserInterface/OrderInterface.java
+++ src/main/java/com/pluralsight/UserInterface/OrderInterface.java
@@ -59,97 +59,108 @@ public class OrderInterface {
     }
 
     public void getChip() {
-        System.out.println("Would you like you like to add chips to your order");
-        String answer = scanner.nextLine().toLowerCase();
-        if (answer.equalsIgnoreCase("yes")) {
-            System.out.println("What Bread would you like your sandwich on?");
-            System.out.println("1) BBQ");
-            System.out.println("2) Original");
-            System.out.println("3) Sour Cream & Onion");
-            System.out.println("4) Sweet Maui Onion");
-            int chipChoice = scanner.nextInt();
-            scanner.nextLine();
-            String chipFlavor = null;
-            switch (chipChoice) {
-                case 1:
-                    chipFlavor = "BBQ";
-                    break;
+        boolean running = true;
+        while (running) {
+            System.out.println("Would you like you like to add chips to your order");
+            String answer = scanner.nextLine().toLowerCase();
+            if (answer.equalsIgnoreCase("yes")) {
+                System.out.println("What Bread would you like your sandwich on?");
+                System.out.println("1) BBQ");
+                System.out.println("2) Original");
+                System.out.println("3) Sour Cream & Onion");
+                System.out.println("4) Sweet Maui Onion");
+                int chipChoice = scanner.nextInt();
+                scanner.nextLine();
+                String chipFlavor = null;
+                switch (chipChoice) {
+                    case 1:
+                        chipFlavor = "BBQ";
+                        break;
 
-                case 2:
-                    chipFlavor = "Original";
-                    break;
-                case 3:
-                    chipFlavor = "Sour Cream & Onion";
-                    break;
-                case 4:
-                    chipFlavor = "Sweet Maui Onion";
-                    break;
-                default:
-                    System.out.println("invalid option");
-                    return;
+                    case 2:
+                        chipFlavor = "Original";
+                        break;
+                    case 3:
+                        chipFlavor = "Sour Cream & Onion";
+                        break;
+                    case 4:
+                        chipFlavor = "Sweet Maui Onion";
+                        break;
+                    default:
+                        System.out.println("invalid option");
+                        return;
 
+                }
+                System.out.println(chipFlavor + " chips have been added to your order.");
+                Chips chips = new Chips(chipFlavor);
+                running = false;
+            } else if (answer.equalsIgnoreCase("no")) {
+                System.out.println("No chips have been added to your order.");
+                running = false;
+            } else {
+                System.out.println("Invalid option. Please respond with 'yes' or 'no'.");
             }
-            System.out.println(chipFlavor + "chips have been added to order. ");
-            Chips chips = new Chips(chipFlavor);
-        } else {
-            System.out.println("No chips have been added to order.");
         }
     }
 
     public void getDrink() {
-        System.out.println("Would you like to add a drink to your order?");
-        String answer = scanner.nextLine();
-        if (answer.equalsIgnoreCase("yes")) {
-            System.out.println("Choose a drink:");
-            System.out.println("1) Cola");
-            System.out.println("2) Lemonade");
-            System.out.println("3) Iced Tea");
-            System.out.println("4) Water");
-            int drinkChoice = scanner.nextInt();
-            scanner.nextLine();
-            String drinkFlavor = null;
+        boolean running = true;
+        while (running) {
+            System.out.println("Would you like to add a drink to your order?");
+            String answer = scanner.nextLine();
+            if (answer.equalsIgnoreCase("yes")) {
+                System.out.println("Choose a drink:");
+                System.out.println("1) Cola");
+                System.out.println("2) Lemonade");
+                System.out.println("3) Iced Tea");
+                System.out.println("4) Water");
+                int drinkChoice = scanner.nextInt();
+                scanner.nextLine();
+                String drinkFlavor = null;
 
-            switch (drinkChoice) {
-                case 1:
-                    drinkFlavor = "Cola";
-                    break;
-                case 2:
-                    drinkFlavor = "Lemonade";
-                    break;
-                case 3:
-                    drinkFlavor = "Iced Tea";
-                    break;
-                case 4:
-                    drinkFlavor = "Water";
-                    break;
-                default:
-                    System.out.println("Invalid option");
-                    return;
-            }
-            System.out.println("What size would you like your? " + drinkFlavor);
-            System.out.println("1) Small");
-            System.out.println("2) Medium");
-            System.out.println("3) Large");
-            int sizeChoice = scanner.nextInt();
-            scanner.nextLine();
+                switch (drinkChoice) {
+                    case 1 -> drinkFlavor = "Cola";
+                    case 2 -> drinkFlavor = "Lemonade";
+                    case 3 -> drinkFlavor = "Iced Tea";
+                    case 4 -> drinkFlavor = "Water";
+                    default -> {
+                        System.out.println("Invalid option");
+                        return;
+                    }
+                }
+                System.out.println("What size would you like your " + drinkFlavor + "?");
+                System.out.println("1) Small");
+                System.out.println("2) Medium");
+                System.out.println("3) Large");
 
-            String drinkSize = null;
-            switch (sizeChoice) {
-                case 1:
-                    drinkSize = "Small";
-                    break;
-                case 2:
-                    drinkSize = "Medium";
-                    break;
-                case 3:
-                    drinkSize = "Large";
-                    break;
-                default:
-                    System.out.println("Invalid option");
-                    return;
+                int sizeChoice = scanner.nextInt();
+                scanner.nextLine();  // Clear buffer
+
+                String drinkSize = null;
+                switch (sizeChoice) {
+                    case 1:
+                        drinkSize = "Small";
+                        break;
+                    case 2:
+                        drinkSize = "Medium";
+                        break;
+                    case 3:
+                        drinkSize = "Large";
+                        break;
+                    default:
+                        System.out.println("Invalid option. Please select a valid size.");
+                        continue;
+                }
+
+                System.out.println(drinkSize + " " + drinkFlavor + " has been added to your order.");
+                Drink drink = new Drink(drinkFlavor, drinkSize);
+                running = false;
+            } else if (answer.equalsIgnoreCase("no")) {
+                System.out.println("No drink has been added to your order.");
+                running = false;
+            } else {
+                System.out.println("Invalid option.");
             }
-            System.out.println(drinkSize+ " "+drinkFlavor+ "has been added to your order");
-            Drink drink = new Drink(drinkFlavor,drinkSize);
         }
     }
 
@@ -164,17 +175,19 @@ public class OrderInterface {
             scanner.nextLine();
             String breadChoice = null;
             switch (answer) {
-                case 1:
+                case 1 -> {
                     return "White";
-                case 2:
+                }
+                case 2 -> {
                     return "Wheat";
-                case 3:
+                }
+                case 3 -> {
                     return "Rye";
-                case 4:
+                }
+                case 4 -> {
                     return "Wrap";
-                default:
-                    System.out.println("Invalid option");
-                    break;
+                }
+                default -> System.out.println("Invalid option");
             }
 
         }
@@ -191,15 +204,16 @@ public class OrderInterface {
             scanner.nextLine();
 
             switch (answer) {
-                case 1:
+                case 1 -> {
                     return 4;
-                case 2:
+                }
+                case 2 -> {
                     return 8;
-                case 3:
+                }
+                case 3 -> {
                     return 12;
-                default:
-                    System.out.println("Invalid option");
-                    break;
+                }
+                default -> System.out.println("Invalid option");
             }
 
         }
@@ -217,13 +231,13 @@ public class OrderInterface {
             scanner.nextLine();
 
             switch (answer) {
-                case 1:
+                case 1 -> {
                     return true;
-                case 2:
+                }
+                case 2 -> {
                     return false;
-                default:
-                    System.out.println("Invalid option");
-                    break;
+                }
+                default -> System.out.println("Invalid option");
             }
         }
 
@@ -252,24 +266,15 @@ public class OrderInterface {
             scanner.nextLine();
             String cheese = null;
             switch (answer) {
-                case 1:
-                    cheese = "American";
-                    break;
-                case 2:
-                    cheese = "Provolone";
-                    break;
-                case 3:
-                    cheese = "Cheddar";
-                    break;
-                case 4:
-                    cheese = "Swiss";
-                    break;
-                case 5:
+                case 1 -> cheese = "American";
+                case 2 -> cheese = "Provolone";
+                case 3 -> cheese = "Cheddar";
+                case 4 -> cheese = "Swiss";
+                case 5 -> {
                     running = false;
                     continue;
-                default:
-                    System.out.println("Invalid option");
-                    break;
+                }
+                default -> System.out.println("Invalid option");
             }
             if (cheese != null) {
                 sandwich.addTopping(new Cheese(cheese));
@@ -295,30 +300,17 @@ public class OrderInterface {
             scanner.nextLine();
             String meat = null;
             switch (answer) {
-                case 1:
-                    meat = "Steak";
-                    break;
-                case 2:
-                    meat = "Ham";
-                    break;
-                case 3:
-                    meat = "Salami";
-                    break;
-                case 4:
-                    meat = "Roast Beef";
-                    break;
-                case 5:
-                    meat = "Chicken";
-                    break;
-                case 6:
-                    meat = "Bacon";
-                    break;
-                case 7:
+                case 1 -> meat = "Steak";
+                case 2 -> meat = "Ham";
+                case 3 -> meat = "Salami";
+                case 4 -> meat = "Roast Beef";
+                case 5 -> meat = "Chicken";
+                case 6 -> meat = "Bacon";
+                case 7 -> {
                     running = false;
                     continue;
-                default:
-                    System.out.println("Invalid option");
-                    break;
+                }
+                default -> System.out.println("Invalid option");
             }
             if (meat != null) {
                 sandwich.addTopping(new Meat(meat));
@@ -347,39 +339,20 @@ public class OrderInterface {
             scanner.nextLine();
             String topping = null;
             switch (answer) {
-                case 1:
-                    topping = "Lettuce";
-                    break;
-                case 2:
-                    topping = "Peppers";
-                    break;
-                case 3:
-                    topping = "Onions";
-                    break;
-                case 4:
-                    topping = "Tomatoes";
-                    break;
-                case 5:
-                    topping = "Jalapenos";
-                    break;
-                case 6:
-                    topping = "Cucumbers";
-                    break;
-                case 7:
-                    topping = "Pickles";
-                    break;
-                case 8:
-                    topping = "Guacamole";
-                    break;
-                case 9:
-                    topping = "Mushrooms";
-                    break;
-                case 10:
+                case 1 -> topping = "Lettuce";
+                case 2 -> topping = "Peppers";
+                case 3 -> topping = "Onions";
+                case 4 -> topping = "Tomatoes";
+                case 5 -> topping = "Jalapenos";
+                case 6 -> topping = "Cucumbers";
+                case 7 -> topping = "Pickles";
+                case 8 -> topping = "Guacamole";
+                case 9 -> topping = "Mushrooms";
+                case 10 -> {
                     running = false;
                     continue;
-                default:
-                    System.out.println("Invalid option");
-                    break;
+                }
+                default -> System.out.println("Invalid option");
             }
             if (topping != null) {
                 sandwich.addTopping(new RegularTopping(topping));
@@ -404,30 +377,17 @@ public class OrderInterface {
             scanner.nextLine();
             String sauce = null;
             switch (answer) {
-                case 1:
-                    sauce = "Mayo";
-                    break;
-                case 2:
-                    sauce = "Mustard";
-                    break;
-                case 3:
-                    sauce = "Ketchup";
-                    break;
-                case 4:
-                    sauce = "Ranch";
-                    break;
-                case 5:
-                    sauce = "Thousand island";
-                    break;
-                case 6:
-                    sauce = "Vinaigrette";
-                    break;
-                case 7:
+                case 1 -> sauce = "Mayo";
+                case 2 -> sauce = "Mustard";
+                case 3 -> sauce = "Ketchup";
+                case 4 -> sauce = "Ranch";
+                case 5 -> sauce = "Thousand island";
+                case 6 -> sauce = "Vinaigrette";
+                case 7 -> {
                     running = false;
                     continue;
-                default:
-                    System.out.println("Invalid option");
-                    break;
+                }
+                default -> System.out.println("Invalid option");
             }
             if (sauce != null) {
                 sandwich.addTopping(new Sauce(sauce));
